b31987b3b63714106604b39434dce2e2089959fb
justin <justin.sneddon@foundry.com>
2018-09-11 15:55:04 +0100

    setting up the test target to include coverage output

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,11 @@
 #include <stdio.h>
 #include "math.h"
 
-class Job
-{
-    
-};
-
-class JobQueue
-{
-    void Push(Job * j);
-    Job * Pop();
-};
-
-void worker1()
-{
-    
-}
-
 int main()
 {
-    
-    std::thread t(&worker1);
     math::vec3 v(1,2,3);
     
     printf("Yo! %f \n",v.len());
-    if(t.joinable())
-        t.join();
     
     return 0;
 }
